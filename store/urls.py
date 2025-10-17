985f79d117c46d71cd63edf58431ab4a1bb12902
nico9696 <nicholasbenedictdysy@gmail.com>
2025-10-17 20:53:40 +0900

    sdf

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 app_name = "store"
-from django.views.static import serve
-from django.urls import re_path
-from django.conf import settings
 
 urlpatterns = [
     path("items/", views.show_products_list, name="show_products_list"), 
@@ -14,7 +11,3 @@ urlpatterns = [
     path("transactions/", views.show_transactions, name="show_transactions"),
     path("selleritems/<int:id>/", views.show_products_of_seller, name="show_products_of_seller")
 ] 
-
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
